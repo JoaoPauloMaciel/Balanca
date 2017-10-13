dee45af665ea736399385f1accd41eb048fbe08a
Laboratório Redes de Computadores CEULP <larc.pmw@labin.canoas>
2017-10-13 19:51:53 -0300

    Autenticação

--- Balanca/autenticacao/views.py
+++ Balanca/autenticacao/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. Essa é a view de autenticao!!!")
+    return HttpResponse("Essa é a view de autenticao!!!")
